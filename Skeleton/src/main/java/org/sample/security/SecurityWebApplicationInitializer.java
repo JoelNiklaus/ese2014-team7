301d16bdf73292d06201527d6d587df26125d266
Joel Niklaus <me@joelniklaus.ch>
2014-10-30 19:20:12 +0100

    Started spring security integration.

---
+++ Skeleton/src/main/java/org/sample/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package org.sample.security;
+
+import org.springframework.security.web.context.*;
+
+public class SecurityWebApplicationInitializer
+      extends AbstractSecurityWebApplicationInitializer {
+}
