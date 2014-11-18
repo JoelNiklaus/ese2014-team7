e9b197433c19c153e5ba11e9f77a61ea70f43aa9
Joel Niklaus <me@joelniklaus.ch>
2014-11-18 16:24:11 +0100

    bugfix.

--- Skeleton/src/main/java/org/sample/controller/service/NotificationServiceImpl.java
+++ Skeleton/src/main/java/org/sample/controller/service/NotificationServiceImpl.java
@@ -27,67 +27,58 @@ public class NotificationServiceImpl implements NotificationService {
 	@Transactional
 	public void saveNotificationsToAffectedUsers(Ad ad) {
 		Notification notification = new Notification();
-		
+
 		Iterable<User> allUsers = userDao.findAll();
 
-		//TODO optimize this very inefficient way of fetching the data.
-		for(User user : allUsers) {
-//			Question: What happens if a user has more than one saved searches
-//			Iterable<Search> allSearches = searchDao.findAll();
-//			
-//			for(Search search : allSearches) {
-//				if(search.getUserId().equals(user.getId())) {
-//					
-//					
-//					
-//				}
-//			}
-			
-			
-			Search search = searchDao.findOne(user.getId());
-			
-			
-			
-			Long priceMin = search.getPriceMin();
-			Long priceMax = search.getPriceMax();
-			Long roomSizeMin = search.getRoomSizeMin();
-			Long roomSizeMax = search.getRoomSizeMax();
-			String city = search.getCity();
-			Iterable<Ad> searchResults;
+		if(userDao.count() > 0)
 
-			//TODO Replace magic Number by config file
-			if(city.equals("")){
-				if(priceMax == 3000){
-					searchResults = adDao.findByRentGreaterThanAndRoomSizeBetween(priceMin-1, roomSizeMin, roomSizeMax);
-				} else if(roomSizeMax==300){
-					searchResults = adDao.findByRentBetweenAndRoomSizeGreaterThan(priceMin, priceMax, roomSizeMin-1);
-				} else if((roomSizeMax == 300) && (priceMax == 3000)){
-					searchResults = adDao.findByRentGreaterThanAndRoomSizeGreaterThan(priceMin, roomSizeMin);
-				} else {
-					searchResults = adDao.findByRentBetweenAndRoomSizeBetween(priceMin, priceMax, roomSizeMin, roomSizeMax);
-				}
-			} else {
-				if(priceMax == 3000){
-					searchResults = adDao.findByRentGreaterThanAndRoomSizeBetweenAndCityLike(priceMin, roomSizeMin, roomSizeMax, city);
-				} else if(roomSizeMax==300){
-					searchResults = adDao.findByRentBetweenAndRoomSizeGreaterThanAndCityLike(priceMin, priceMax, roomSizeMin, city);
-				} else if((roomSizeMax == 300) && (priceMax == 3000)){
-					searchResults = adDao.findByRentGreaterThanAndRoomSizeGreaterThanAndCityLike(priceMin, roomSizeMin, city);
-				} else {
-					searchResults = adDao.findByRentBetweenAndRoomSizeBetweenAndCityLike(priceMin, priceMax, roomSizeMin, roomSizeMax, city);
+			//TODO optimize this very inefficient way of fetching the data.
+			for(User user : allUsers) {
+				//TODO Question: What happens if a user has more than one saved searches
+				Search search = searchDao.findOne(user.getId());
+
+				if(search != null) {
+					Long priceMin = search.getPriceMin();
+					Long priceMax = search.getPriceMax();
+					Long roomSizeMin = search.getRoomSizeMin();
+					Long roomSizeMax = search.getRoomSizeMax();
+					String city = search.getCity();
+					Iterable<Ad> searchResults;
+
+					//TODO Replace magic Number by config file
+					if(city.equals("")){
+						if(priceMax == 3000){
+							searchResults = adDao.findByRentGreaterThanAndRoomSizeBetween(priceMin-1, roomSizeMin, roomSizeMax);
+						} else if(roomSizeMax==300){
+							searchResults = adDao.findByRentBetweenAndRoomSizeGreaterThan(priceMin, priceMax, roomSizeMin-1);
+						} else if((roomSizeMax == 300) && (priceMax == 3000)){
+							searchResults = adDao.findByRentGreaterThanAndRoomSizeGreaterThan(priceMin, roomSizeMin);
+						} else {
+							searchResults = adDao.findByRentBetweenAndRoomSizeBetween(priceMin, priceMax, roomSizeMin, roomSizeMax);
+						}
+					} else {
+						if(priceMax == 3000){
+							searchResults = adDao.findByRentGreaterThanAndRoomSizeBetweenAndCityLike(priceMin, roomSizeMin, roomSizeMax, city);
+						} else if(roomSizeMax==300){
+							searchResults = adDao.findByRentBetweenAndRoomSizeGreaterThanAndCityLike(priceMin, priceMax, roomSizeMin, city);
+						} else if((roomSizeMax == 300) && (priceMax == 3000)){
+							searchResults = adDao.findByRentGreaterThanAndRoomSizeGreaterThanAndCityLike(priceMin, roomSizeMin, city);
+						} else {
+							searchResults = adDao.findByRentBetweenAndRoomSizeBetweenAndCityLike(priceMin, priceMax, roomSizeMin, roomSizeMax, city);
+						}
+					}
+
+					for(Ad result : searchResults)
+						if(result.equals(ad)) {
+							notification.setAdId(ad.getId());
+							notification.setUserId(user.getId());
+							notificationDao.save(notification);
+							break;
+						}
 				}
 			}
-			
-			for(Ad result : searchResults)
-				if(result.equals(ad)) {
-					notification.setAdId(ad.getId());
-					notification.setUserId(user.getId());
-					notificationDao.save(notification);
-					break;
-				}
-		}
 	}
-	
+
 	@Transactional
 	public Iterable<Notification> findNotifications(User user) {
 		Iterable<Notification> allnotifications = notificationDao.findAll();
@@ -102,11 +93,11 @@ public class NotificationServiceImpl implements NotificationService {
 
 		return (Iterable<Notification>)results;
 	}
-	
+
 	@Transactional
 	public Notification removeNotification(Notification notification) {
 		notificationDao.delete(notification);
-		
+
 		return notification;
 	}
 
